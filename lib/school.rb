bbdca9415519fbb31d6eea675cb914353b076aa8
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-27 23:47:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+# code here!# code here!
+require 'pry'
+class School
+  attr_reader :name, :roster, :grades
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+      @roster
+    end
+  end
+
+  def grade(grade)
+    @grades = @roster[grade]
+    @grades
+  end
+
+  def sort
+    @roster.sort_by { |grade, names_array| names_array.sort}
+binding.pry
+  end
+end
